19a42989109e28cf4e74de414eebdc9f72a242b1
Coderofcoders70 <Lakshayas918@gmail.com>
2025-12-30 00:23:28 +0530

    Added frontend mock-data

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css';
 import { useEffect, useState } from 'react';
 import ChatInput from './components/ChatInput';
 import ChatMessages from './components/ChatMessages';
+import { Chatbot } from 'supersimpledev';
 
 function App() {
 
@@ -20,10 +21,25 @@ function App() {
     localStorage.setItem('messages', JSON.stringify(chatMessages));
   }, [chatMessages]);
 
+  useEffect(() => {
+    Chatbot.addResponses({
+      'i am fine': "That's great!! How can I help You ?",
+      'goodbye': "Okay!! If you want to know something don't hesitate to reach",
+      'What can you do': "I know how to flip a coin, roll a dice, or get today's date. Let me know how I can help!",
+      'give me an unique Id': function () {
+        return `Sure here's an unique Id: ${crypto.randomUUID()}`
+      }
+    });
+  }, []);
+
+  let total = null;
+  const num = total += chatMessages.length;
+  const title = `${num} Messages`;
+
   return (
     <>
       <link rel="icon" type="image/svg+xml" href='src/assets/robot.png' />
-      <title>Smartbot</title>
+      <title>{title}</title>
 
       <div className="app-container">
         {chatMessages.length === 0 && (
